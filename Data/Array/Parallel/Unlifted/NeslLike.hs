b97697c6162db875ce07ec98a23f34019eff0eb3
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2006-04-06 17:33:07 +0000

    Add NeslLike.hs (which I forgot before)

---
+++ Data/Array/Parallel/Unlifted/NeslLike.hs
@@ -0,0 +1,165 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Data.Array.Parallel.Unlifted.Listlike
+-- Copyright   : (c) [2001..2002] Manuel M T Chakravarty & Gabriele Keller
+--		 (c) 2006         Manuel M T Chakravarty
+-- License     : see libraries/base/LICENSE
+-- 
+-- Maintainer  : Manuel M T Chakravarty <chak@cse.unsw.edu.au>
+-- Stability   : internal
+-- Portability : portable
+--
+-- Description ---------------------------------------------------------------
+--
+--  Unlifted array versions of Nesl-like combinators.
+--
+-- Todo ----------------------------------------------------------------------
+--
+
+module Data.Array.Parallel.Unlifted.NeslLike (
+  -- * Nesl-like combinators
+  flattenU, (>:), segmentU, toU, toSU, fromU, emptyU, sliceU,
+  permuteU, bpermuteU, bpermuteSU, bpermuteDftU, {-crossU, indexOfU -}
+) where
+
+-- friends
+import Data.Array.Parallel.Base.Hyperstrict
+import Data.Array.Parallel.Base.BUArr (
+  indexBU, runST)
+import Data.Array.Parallel.Monadic.UArr (
+  UA, MUA, UArr, lengthU, indexU, sliceU, toUSegd, newMU, writeMU,
+  unsafeFreezeMU, (>:), flattenU, psumUS) 
+import Data.Array.Parallel.Declarative.Loop (
+  replicateU, loopU, replicateSU, loopSU,
+  loopArr, loopArrS, loopAcc, loopAccS, loopSndAcc)
+import Data.Array.Parallel.Declarative.Fusion (
+  noEFL, noSFL, noAL, mapEFL, filterEFL, foldEFL, scanEFL, transSFL, keepSFL)
+import Data.Array.Parallel.Unlifted.ListLike
+  ((!:))
+
+
+-- |Segmentation
+-- -------------
+
+-- |Segment an array according to the segmentation of the first argument
+--
+segmentU :: (UA e', UA e) => UArr (UArr e') -> UArr e -> UArr (UArr e)
+{-# INLINE segmentU #-}
+segmentU template arr = fst (flattenU template) >: arr
+
+
+-- |Conversion
+-- -----------
+
+-- |Turn a list into a parallel array
+--
+toU :: MUA e => [e] -> UArr e
+{-# INLINE toU #-}
+toU l = 
+  loopArr $ 
+    loopU (\(x:xs) (_::()) -> (xs, Just $ x)) l (replicateU (length l) noAL)
+
+-- |Turn a nested list into a segmented parallel array
+--
+toSU :: MUA e => [[e]] -> UArr (UArr e)
+{-# INLINE toSU #-}
+toSU ls = let lens = toU $ map length ls
+	      a    = toU $ concat ls
+          in
+	  toUSegd lens >: a
+
+-- |Collect the elements of a parallel array in a list
+--
+fromU :: UA e => UArr e -> [e]
+{-# INLINE fromU #-}
+fromU a = [a!:i | i <- [0..lengthU a - 1]]
+
+-- |Yield an empty array
+--
+emptyU :: MUA e => UArr e
+emptyU = runST (do
+	   mpa <- newMU 0
+	   unsafeFreezeMU mpa 0
+         )
+
+-- sliceU is re-exported from UArr
+
+
+-- |Permutations
+-- -------------
+
+-- |Standard permutation
+--
+permuteU :: MUA e => UArr e -> UArr Int -> UArr e
+{-# INLINE permuteU #-}
+permuteU arr is = 
+  runST (do
+    mpa <- newMU n
+    permute0 mpa
+    unsafeFreezeMU mpa n
+  )
+  where
+    n		 = lengthU arr
+    permute0 mpa = permute 0
+      where
+        permute i 
+	  | i == n    = return ()
+	  | otherwise = writeMU mpa (is!:i) (arr!:i) >> permute (i + 1)
+
+-- |Back permutation operation (ie, the permutation vector determines for each
+-- position in the result array its origin in the input array)
+--
+bpermuteU :: MUA e => UArr e -> UArr Int -> UArr e
+{-# INLINE bpermuteU #-}
+bpermuteU a = loopArr . loopU (mapEFL (a !:)) noAL
+
+-- |Segmented back permute
+--
+bpermuteSU :: MUA e => UArr (UArr e) -> UArr (UArr Int) -> UArr (UArr e)
+{-# INLINE bpermuteSU #-}
+bpermuteSU as = loopArrS . loopSU extract nextOff 0
+	        where
+		  (segd, a) = flattenU as
+		  psum	    = psumUS segd
+		  --
+	          extract off i = (off, Just $ a!:(off + i))
+		  --
+		  nextOff _ segi = (psum `indexBU` (segi + 1), 
+				    Nothing::Maybe ())
+
+-- |Default back permute
+--
+-- * The values of the index-value pairs are written into the position in the
+--   result array that is indicated by the corresponding index.
+--
+-- * All positions not covered by the index-value pairs will have the value
+--   determined by the initialiser function for that index position.
+--
+bpermuteDftU :: MUA e
+	     => Int			        -- |length of result array
+	     -> (Int -> e)		        -- |initialiser function
+	     -> UArr (Int :*: e)		-- |index-value pairs
+	     -> UArr e
+{-# INLINE bpermuteDftU #-}
+bpermuteDftU n init arr =
+  runST (do
+    mpa <- newMU n
+    doInit0  mpa
+    permute0 mpa
+    unsafeFreezeMU mpa n
+  )
+  where
+    doInit0 mpa = doInit 0
+      where
+        doInit i | i == n    = return ()
+		 | otherwise = writeMU mpa i (init i) >> doInit (i + 1)
+    --
+    m		 = lengthU arr
+    permute0 mpa = permute 0
+      where
+        permute i 
+	  | i == m    = return ()
+	  | otherwise = do
+			  let (j :*: e) = arr!:i
+			  writeMU mpa j e
+			  permute (i + 1)
