f53983b2301fe95e12aa571716c8e15dfffe0974
rl@cse.unsw.edu.au <unknown>
2006-05-04 02:46:51 +0000

    Actually add Base.hs to darcs

---
+++ Data/Array/Parallel/Base.hs
@@ -0,0 +1,29 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Data.Array.Parallel.Base
+-- Copyright   : (c) 2006         Roman Leshchinskiy
+-- License     : see libraries/base/LICENSE
+-- 
+-- Maintainer  : Roman Leshchinskiy <rl@cse.unsw.edu.au>
+-- Stability   : internal
+-- Portability : non-portable (unboxed values and GHC libraries)
+--
+-- Description ---------------------------------------------------------------
+--
+-- Interface to the Base modules
+--
+
+module Data.Array.Parallel.Base (
+  module Data.Array.Parallel.Base.Debug,
+  module Data.Array.Parallel.Base.Hyperstrict,
+  module Data.Array.Parallel.Base.Fusion,
+
+  ST(..), runST
+) where
+
+import Data.Array.Parallel.Base.Debug
+import Data.Array.Parallel.Base.Hyperstrict
+import Data.Array.Parallel.Base.Fusion
+
+import GHC.ST (ST(..), runST)
+
