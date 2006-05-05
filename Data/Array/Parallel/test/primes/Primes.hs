eb3fdb6408520b371aaa398dd74a0ba017475c48
rl@cse.unsw.edu.au <unknown>
2006-05-05 14:31:32 +0000

    Convert the primes benchmark (seems to work)

--- Data/Array/Parallel/test/primes/Primes.hs
+++ Data/Array/Parallel/test/primes/Primes.hs
@@ -13,42 +13,43 @@ import System
 import Control.Exception (evaluate)
 
 -- Parallel array support
-import PArray
+import Data.Array.Parallel.Unlifted
 import BenchUtils
 
 import Debug.Trace
 
+xtrace s x = x
 
 -- Benchmarked code
 -- ----------------
 
-primes :: Int -> PArrInt
+primes :: Int -> UArr Int
 {-# NOINLINE primes #-}
 primes n 
 --  | n <= 2    = emptyP
-  | n <= 2    = trace "emptyP" emptyP
+  | n <= 2    = xtrace "emptyP" emptyU
   | otherwise = 
     let
 --      sqrPrimes = primes (ceiling (sqrt (fromIntegral n)))
       sqrPrimes = primes (
 		    let 
-		      f = fromIntegral (trace ("n = "++show n)n)
-		      c = ceiling (sqrt (trace ("f = "++show f) f))
+		      f = fromIntegral (xtrace ("n = "++show n)n)
+		      c = ceiling (sqrt (xtrace ("f = "++show f) f))
 		    in
-		    trace ("c = "++show c) c)
+		    xtrace ("c = "++show c) c)
 		    
-      sieves    = concatP $
+      sieves    = concatSU $
 --		    trace "sieves"$
 -- doesn't die when the above trace is activated or when compiled with -Onot
 -- should try running with bounds checks activated
-		    enumFromThenToSP 
-		      (mapP (*2) sqrPrimes)
-		      (mapP (*3) sqrPrimes)
-		      (replicateP (lengthP sqrPrimes) (n - 1))
-      sieves'   = zipP sieves (replicateP (lengthP sieves) False)
-      flags     = bpermuteDftP n (const True) sieves'
+		    enumFromThenToSU
+		      (mapU (*2) sqrPrimes)
+		      (mapU (*3) sqrPrimes)
+		      (replicateU (lengthU sqrPrimes) (n - 1))
+      sieves'   = zipU sieves (replicateU (lengthU sieves) False)
+      flags     = bpermuteDftU n (const True) sieves'
     in
-    dropP 2 (filterP (flags!:) (enumFromToP 0 (n - 1)))
+    dropU 2 (filterU (flags!:) (enumFromToU 0 (n - 1)))
 
 
 -- H98 array version for comparison
@@ -79,7 +80,7 @@ primes_bench size =
   timeIt ("N = " ++ show size) $
     do
       let result = primes size			-- won't be fused due...
-	  sum    = sumP result			-- ...to the NOINLINE
+	  sum    = sumU result			-- ...to the NOINLINE
       evaluate $ sum			        -- make sure the work is done
       return   $ show sum
 
@@ -105,8 +106,8 @@ main =
 
     doBenchmarks [(primes_bench_h98, "H98", "h98"),
 		  (primes_bench    , "PArr", "parr")]
---		 [100000]
-		 [5]
+		 [100000]
+--		 [5]
 		 ("Primes up to 100,000\n\
 		  \on " ++ host)
 		 "primes"
