7f90f7faeef403773e924f3122cabd56343cb6b7
rl@cse.unsw.edu.au <unknown>
2006-11-16 01:53:48 +0000

    Forgot to add...

---
+++ Data/Array/Parallel/Unlifted/Parallel/Segmented.hs
@@ -0,0 +1,34 @@
+module Data.Array.Parallel.Unlifted.Parallel.Segmented (
+  zipWithSUP, foldlSUP, foldSUP, sumSUP, bpermuteSUP'
+) where
+
+import Data.Array.Parallel.Unlifted.Flat
+import Data.Array.Parallel.Unlifted.Segmented
+import Data.Array.Parallel.Unlifted.Distributed
+
+zipWithSUP :: (UA a, UA b, UA c)
+           => (a -> b -> c) -> SUArr a -> SUArr b -> SUArr c
+{-# INLINE zipWithSUP #-}
+zipWithSUP f as bs = joinSD   theGang balanced
+                   $ zipWithD theGang (zipWithSU f)
+                       (splitSD theGang balanced as)
+                       (splitSD theGang balanced bs)
+
+foldlSUP :: (UA a, UA b) => (b -> a -> b) -> b -> SUArr a -> UArr b
+{-# INLINE foldlSUP #-}
+foldlSUP f z = joinD   theGang unbalanced
+             . mapD    theGang (foldlSU f z)
+             . splitSD theGang unbalanced
+
+foldSUP :: (UA a, UA b) => (b -> a -> b) -> b -> SUArr a -> UArr b
+{-# INLINE foldSUP #-}
+foldSUP = foldlSUP
+
+sumSUP :: (Num e, UA e) => SUArr e -> UArr e
+{-# INLINE sumSUP #-}
+sumSUP = foldSUP (+) 0
+
+bpermuteSUP' :: UA a => UArr a -> SUArr Int -> SUArr a
+{-# INLINE bpermuteSUP' #-}
+bpermuteSUP' as = splitJoinSD theGang (bpermuteSD' theGang as)
+
