bf29246d94bf2924bf8778859f179da645194bde
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2006-11-16 07:50:55 +0000

    Undo the unstreamMU join point workaround
    It creates more problems than it solves. We'll just have to bump the inlining
    threshold for now. At the moment, smvm compiles nicely with
    -fmax-simplifier-iterations6 -funfolding-use-threshold20

--- Data/Array/Parallel/Unlifted/Flat/Stream.hs
+++ Data/Array/Parallel/Unlifted/Flat/Stream.hs
@@ -52,18 +52,9 @@ unstreamMU marr (Stream next s n) = fill s 0
                case next s of
                  Done       -> return i
                  Skip s'    -> fill (rebox s') i
-
-                 -- The following INLINE is necessary to work around the join
-                 -- point problem. In complex pipelines, where a Yield is
-                 -- produced if one of several conditions applies, GHC will
-                 -- create a join point for the alternative. The join point 
-                 -- *must* be inlined; otherwise, SpecConstr won't work. The
-                 -- INLINE ensures this (hopefully).
-                 Yield x s' -> let {-# INLINE p #-}
-                                   p = do
-                                     writeMU marr i x
-                                     fill (rebox s') (i+1)
-                               in p
+                 Yield x s' -> do
+                                 writeMU marr i x
+                                 fill (rebox s') (i+1)
 
 -- | Fusion rules
 -- --------------
