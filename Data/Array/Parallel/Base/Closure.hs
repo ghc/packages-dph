1c9177d29d3d4e865833534b7d69692714847546
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-04-02 01:22:47 +0000

    Add INLINE pragmas

--- Data/Array/Parallel/Base/Closure.hs
+++ Data/Array/Parallel/Base/Closure.hs
@@ -8,8 +8,10 @@ infixr 0 $:
 data a :-> b = forall e. Clo !(e -> a -> b) e
 
 closure :: (e -> a -> b) -> e -> (a :-> b)
+{-# INLINE closure #-}
 closure = Clo
 
 ($:) :: (a :-> b) -> a -> b
+{-# INLINE ($:) #-}
 Clo f e $: x = f e x
 
