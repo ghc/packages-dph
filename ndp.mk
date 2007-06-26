dad91d40cc1707b3d77ce8251038cc74880eaa2a
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-06-26 02:19:00 +0000

    Add flags to ndp.mk

--- ndp.mk
+++ ndp.mk
@@ -1,4 +1,5 @@
 NDPFLAGS = -fglasgow-exts -fbang-patterns -O2 -funbox-strict-fields\
            -fliberate-case-threshold100 -fdicts-cheap -fno-method-sharing\
-           -fmax-simplifier-iterations6 -threaded
+           -fmax-simplifier-iterations6 -threaded -ftype-families\
+	   -fcpr-off
 
