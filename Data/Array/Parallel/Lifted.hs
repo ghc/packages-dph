fed8b66e4a63855f43bf32eec064b7a8e989bf7a
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-08-07 06:22:39 +0000

    Export dPA_Clo

--- Data/Array/Parallel/Lifted.hs
+++ Data/Array/Parallel/Lifted.hs
@@ -2,7 +2,7 @@ module Data.Array.Parallel.Lifted (
   module Data.Array.Parallel.Lifted.PArray,
   module Data.Array.Parallel.Lifted.Instances,
 
-  (:->), ($:), ($:^)
+  (:->), ($:), ($:^), dPA_Clo
 ) where
 
 import Data.Array.Parallel.Lifted.PArray
