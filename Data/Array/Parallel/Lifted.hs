e6dec6e86a457145e5582e98f42ed8dd1e576518
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-08-30 02:59:46 +0000

    Remove Lifted.Utils

--- Data/Array/Parallel/Lifted.hs
+++ Data/Array/Parallel/Lifted.hs
@@ -1,7 +1,6 @@
 module Data.Array.Parallel.Lifted (
   module Data.Array.Parallel.Lifted.PArray,
   module Data.Array.Parallel.Lifted.Instances,
-  module Data.Array.Parallel.Lifted.Utils,
 
   (:->), ($:), ($:^), dPA_Clo,
 ) where
@@ -9,5 +8,4 @@ module Data.Array.Parallel.Lifted (
 import Data.Array.Parallel.Lifted.PArray
 import Data.Array.Parallel.Lifted.Closure
 import Data.Array.Parallel.Lifted.Instances
-import Data.Array.Parallel.Lifted.Utils
 
