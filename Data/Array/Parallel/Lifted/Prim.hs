215051ceccfce6d36d9ce15c718280f8259214a0
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-11-17 01:54:42 +0000

    Add INLINE pragmas

--- Data/Array/Parallel/Lifted/Prim.hs
+++ Data/Array/Parallel/Lifted/Prim.hs
@@ -16,26 +16,34 @@ newtype PArray_Int# = PInt# (UArr Int)
 
 lengthPA_Int# :: PArray_Int# -> Int#
 lengthPA_Int# (PInt# arr) = case lengthU arr of { I# n# -> n# }
+{-# INLINE lengthPA_Int# #-}
 
 emptyPA_Int# :: PArray_Int#
 emptyPA_Int# = PInt# emptyU
+{-# INLINE emptyPA_Int# #-}
 
 replicatePA_Int# :: Int# -> Int# -> PArray_Int#
 replicatePA_Int# n# i# = PInt# (replicateU (I# n#) (I# i#))
+{-# INLINE replicatePA_Int# #-}
 
 upToPA_Int# :: Int# -> PArray_Int#
 upToPA_Int# n# = PInt# (enumFromToU 0 ((I# n#) -1))
+{-# INLINE upToPA_Int# #-}
 
 selectPA_Int# :: PArray_Int# -> Int# -> PArray_Bool#
 selectPA_Int# (PInt# ns) i# = PBool# (mapU (\n -> n == I# i#) ns)
+{-# INLINE selectPA_Int# #-}
 
 newtype PArray_Bool# = PBool# (UArr Bool)
 
 lengthPA_Bool# :: PArray_Bool# -> Int#
 lengthPA_Bool# (PBool# arr) = case lengthU arr of { I# n# -> n# }
+{-# INLINE lengthPA_Bool# #-}
 
 truesPA_Bool# :: PArray_Bool# -> Int#
 truesPA_Bool# (PBool# arr)
   = case sumU (mapU (\b -> if b then 1 else 0) arr) of
       I# n# -> n#
+{-# INLINE truesPA_Bool# #-}
+
 
