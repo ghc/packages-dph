d095813607d8e9ee1da7dea64da150b13f93d7e4
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-11-20 02:44:38 +0000

    Temporary hack for passing PArrays from unvectorised to vectorised code

--- Data/Array/Parallel/Prelude.hs
+++ Data/Array/Parallel/Prelude.hs
@@ -1,8 +1,24 @@
+{-# LANGUAGE PArr #-}
+
 module Data.Array.Parallel.Prelude (
     module Data.Array.Parallel.Prelude.PArr
   , module Data.Array.Parallel.Prelude.Int
+  
+  , PArray, fromPArrayP, fromPArrayPA
 ) where
 
 import Data.Array.Parallel.Prelude.PArr
 import Data.Array.Parallel.Prelude.Int
 
+import Data.Array.Parallel.Lifted.PArray
+import Data.Array.Parallel.Lifted.Closure
+import Data.Array.Parallel.Lifted.Combinators
+
+fromPArrayP :: PArray a -> [:a:]
+{-# NOINLINE fromPArrayP #-}
+fromPArrayP _ = error "fromPArray"
+
+fromPArrayPA :: PA a -> PArray a :-> PArray a
+{-# INLINE fromPArrayPA #-}
+fromPArrayPA pa = closure1 (\x -> x) (\xs -> xs)
+ 
