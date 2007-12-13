3a63bab06902834796f8716787bf4440d775119a
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-12-13 00:10:45 +0000

    Add unsafe_fold1Index

--- Data/Array/Parallel/Lifted/Prim.hs
+++ Data/Array/Parallel/Lifted/Prim.hs
@@ -5,6 +5,9 @@ import Data.Array.Parallel.Unlifted
 import Data.Array.Parallel.Lifted.PArray
 import Data.Array.Parallel.Lifted.Unboxed
 import Data.Array.Parallel.Lifted.Repr
+import Data.Array.Parallel.Lifted.Instances
+
+import Data.Array.Parallel.Base.Hyperstrict (pairS, unpairS)
 
 unsafe_map :: (PrimPA a, PrimPA b) => (a -> b) -> PArray a -> PArray b
 {-# INLINE unsafe_map #-}
@@ -37,4 +40,25 @@ unsafe_fold1s :: PrimPA a => (a -> a -> a) -> PArray (PArray a) -> PArray a
 unsafe_fold1s f xss = fromUArrPA (prim_lengthPA (concatPA# xss))
                     . fold1SU f
                     $ toSUArrPA xss
+
+unsafe_fold1Index :: PrimPA a
+                  => ((Int, a) -> (Int, a) -> (Int, a)) -> PArray a -> Int
+{-# INLINE unsafe_fold1Index #-}
+unsafe_fold1Index f = fstS . fold1U f' . indexedU . toUArrPA
+  where
+    {-# INLINE f' #-}
+    f' p q = pairS $ f (unpairS p) (unpairS q)
+
+unsafe_fold1sIndex :: PrimPA a
+                   => ((Int, a) -> (Int, a) -> (Int, a))
+                   -> PArray (PArray a) -> PArray Int
+{-# INLINE unsafe_fold1sIndex #-}
+unsafe_fold1sIndex f xss = fromUArrPA (nested_lengthPA xss) 
+                         . fstU
+                         . fold1SU f'
+                         . indexedSU
+                         $ toSUArrPA xss
+  where
+    {-# INLINE f' #-}
+    f' p q = pairS $ f (unpairS p) (unpairS q)
 
