9d277c1ac38f302be8684c409b84c55b06e9c417
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2008-01-24 00:07:31 +0000

    Fiddle with INLINEs

--- Data/Array/Parallel/Lifted/Prim.hs
+++ Data/Array/Parallel/Lifted/Prim.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE CPP #-}
+
+#include "fusion-phases.h"
+
 module Data.Array.Parallel.Lifted.Prim
 where
 
@@ -10,40 +14,40 @@ import Data.Array.Parallel.Lifted.Instances
 import Data.Array.Parallel.Base.Hyperstrict (pairS, unpairS)
 
 unsafe_map :: (PrimPA a, PrimPA b) => (a -> b) -> PArray a -> PArray b
-{-# INLINE unsafe_map #-}
+{-# INLINE_PA unsafe_map #-}
 unsafe_map f xs = fromUArrPA (prim_lengthPA xs)
                 . mapU f
                 $ toUArrPA xs
 
 unsafe_zipWith :: (PrimPA a, PrimPA b, PrimPA c)
                => (a -> b -> c) -> PArray a -> PArray b -> PArray c
-{-# INLINE unsafe_zipWith #-}
+{-# INLINE_PA unsafe_zipWith #-}
 unsafe_zipWith f xs ys = fromUArrPA (prim_lengthPA xs)
                        $ zipWithU f (toUArrPA xs) (toUArrPA ys)
 
 unsafe_fold :: PrimPA a => (a -> a -> a) -> a -> PArray a -> a
-{-# INLINE unsafe_fold #-}
+{-# INLINE_PA unsafe_fold #-}
 unsafe_fold f z = foldU f z . toUArrPA
 
 unsafe_fold1 :: PrimPA a => (a -> a -> a) -> PArray a -> a
-{-# INLINE unsafe_fold1 #-}
+{-# INLINE_PA unsafe_fold1 #-}
 unsafe_fold1 f = fold1U f . toUArrPA
 
 unsafe_folds :: PrimPA a => (a -> a -> a) -> a -> PArray (PArray a) -> PArray a
-{-# INLINE unsafe_folds #-}
+{-# INLINE_PA unsafe_folds #-}
 unsafe_folds f z xss = fromUArrPA (prim_lengthPA (concatPA# xss))
                      . foldSU f z
                      $ toSUArrPA xss
 
 unsafe_fold1s :: PrimPA a => (a -> a -> a) -> PArray (PArray a) -> PArray a
-{-# INLINE unsafe_fold1s #-}
+{-# INLINE_PA unsafe_fold1s #-}
 unsafe_fold1s f xss = fromUArrPA (prim_lengthPA (concatPA# xss))
                     . fold1SU f
                     $ toSUArrPA xss
 
 unsafe_fold1Index :: PrimPA a
                   => ((Int, a) -> (Int, a) -> (Int, a)) -> PArray a -> Int
-{-# INLINE unsafe_fold1Index #-}
+{-# INLINE_PA unsafe_fold1Index #-}
 unsafe_fold1Index f = fstS . fold1U f' . indexedU . toUArrPA
   where
     {-# INLINE f' #-}
@@ -52,7 +56,7 @@ unsafe_fold1Index f = fstS . fold1U f' . indexedU . toUArrPA
 unsafe_fold1sIndex :: PrimPA a
                    => ((Int, a) -> (Int, a) -> (Int, a))
                    -> PArray (PArray a) -> PArray Int
-{-# INLINE unsafe_fold1sIndex #-}
+{-# INLINE_PA unsafe_fold1sIndex #-}
 unsafe_fold1sIndex f xss = fromUArrPA (nested_lengthPA xss) 
                          . fstU
                          . fold1SU f'
