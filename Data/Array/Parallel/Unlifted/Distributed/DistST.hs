b72bf0dd961b345dd9a8dbc09cc830a435593cc7
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2008-01-24 00:08:05 +0000

    Add INLINE pragmas

--- Data/Array/Parallel/Unlifted/Distributed/DistST.hs
+++ Data/Array/Parallel/Unlifted/Distributed/DistST.hs
@@ -36,42 +36,52 @@ import Control.Monad (liftM)
 newtype DistST s a = DistST { unDistST :: Int -> ST s a }
 
 instance Monad (DistST s) where
+  {-# INLINE return #-}
   return         = DistST . const . return 
+
+  {-# INLINE (>>=) #-}
   DistST p >>= f = DistST $ \i -> do
                                     x <- p i
                                     unDistST (f x) i
 
 -- | Yields the index of the current thread within its gang.
 myIndex :: DistST s Int
+{-# INLINE myIndex #-}
 myIndex = DistST return
 
 -- | Lifts an 'ST' computation into the 'DistST' monad. The lifted computation
 -- should be data parallel.
 stToDistST :: ST s a -> DistST s a
+{-# INLINE stToDistST #-}
 stToDistST p = DistST $ \i -> p
 
 -- | Yields the 'Dist' element owned by the current thread.
 myD :: DT a => Dist a -> DistST s a
+{-# INLINE myD #-}
 myD dt = liftM (indexD dt) myIndex
 
 -- | Yields the 'MDist' element owned by the current thread.
 readMyMD :: DT a => MDist a s -> DistST s a
+{-# INLINE readMyMD #-}
 readMyMD mdt = do
                  i <- myIndex
                  stToDistST $ readMD mdt i
 
 -- | Writes the 'MDist' element owned by the current thread.
 writeMyMD :: DT a => MDist a s -> a -> DistST s ()
+{-# INLINE writeMyMD #-}
 writeMyMD mdt x = do
                     i <- myIndex
                     stToDistST $ writeMD mdt i x
 
 -- | Execute a data-parallel computation on a 'Gang'.
 distST_ :: Gang -> DistST s () -> ST s ()
+{-# INLINE distST_ #-}
 distST_ g = gangST g . unDistST
 
 -- | Execute a data-parallel computation, yielding the distributed result.
 distST :: DT a => Gang -> DistST s a -> ST s (Dist a)
+{-# INLINE distST #-}
 distST g p = do
                md <- newMD g
                distST_ g $ writeMyMD md =<< p
@@ -79,5 +89,6 @@ distST g p = do
 
 -- | Run a data-parallel computation, yielding the distributed result.
 runDistST :: DT a => Gang -> (forall s. DistST s a) -> Dist a
+{-# INLINE runDistST #-}
 runDistST g p = runST (distST g p)
 
