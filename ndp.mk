b664ba32ede173ed8f1790d5e4108b9722c799b7
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2008-01-31 02:55:51 +0000

    Bump max-simplifier-iterations to 20

--- ndp.mk
+++ ndp.mk
@@ -1,5 +1,5 @@
 NDPFLAGS = -fglasgow-exts -fbang-patterns -O2 -funbox-strict-fields\
            -fdicts-cheap -fno-method-sharing -fno-spec-constr-threshold\
-           -fmax-simplifier-iterations10 -threaded -XTypeFamilies\
+           -fmax-simplifier-iterations20 -threaded -XTypeFamilies\
 	   -fcpr-off
 
