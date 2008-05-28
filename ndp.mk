0b7759624dc7647d187126b9dd7b7b9245969717
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2008-05-28 04:50:12 +0000

    Use -Odph

--- ndp.mk
+++ ndp.mk
@@ -1,5 +1,3 @@
-NDPFLAGS = -fglasgow-exts -fbang-patterns -O2 -funbox-strict-fields\
-           -fdicts-cheap -fno-method-sharing -fno-spec-constr-threshold\
-           -fmax-simplifier-iterations20 -threaded -XTypeFamilies\
-	   -fcpr-off
+NDPFLAGS = -fglasgow-exts -fbang-patterns -Odph -funbox-strict-fields\
+           -threaded -XTypeFamilies -fcpr-off
 
