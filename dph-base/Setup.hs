d8a98caed333f1db0ff185d4ae97acca59b2edc6
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2008-07-04 02:14:54 +0000

    Disable Haddock in dph-base
    Haddock < 2 can't handle infix type constructors

--- dph-base/Setup.hs
+++ dph-base/Setup.hs
@@ -1,3 +1,9 @@
 import Distribution.Simple
-main = defaultMain
+import System.IO
+main = defaultMainWithHooks
+     $ simpleUserHooks {
+         haddockHook = \_ _ _ _ -> warn
+     }
+  where
+    warn = hPutStrLn stderr "WARNING: ndp not supported by hadock"
 
