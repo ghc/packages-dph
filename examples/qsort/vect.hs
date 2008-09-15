805e6c0ffbf5cc5c4b2cc1cb968a912b18525c28
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2008-09-15 02:22:01 +0000

    Use new stuff in qsort

--- examples/qsort/vect.hs
+++ examples/qsort/vect.hs
@@ -5,25 +5,21 @@ import Control.Exception (evaluate)
 import System.Console.GetOpt
 import qualified System.Random as R
 
-import Data.Array.Parallel.Prelude (PArray, toUArrPA, fromUArrPA')
-import qualified Data.Array.Parallel.Unlifted as U
+import Data.Array.Parallel.PArray (PArray)
+import qualified Data.Array.Parallel.PArray as P
 
 import Bench.Benchmark
 import Bench.Options
 
 import Debug.Trace
 
-qsortVect' :: U.Array Double -> U.Array Double
-qsortVect' xs = res
-  where res = toUArrPA . qsortVect $ fromUArrPA' xs
-
-generateVector :: Int -> IO (Point (U.Array Double))
+generateVector :: Int -> IO (Point (PArray Double))
 generateVector n =
   do
-    evaluate vec
+    evaluate (P.nf vec)
     return $ ("N = " ++ show n) `mkPoint` vec
   where
-    vec = U.fromList (reverse [1..fromInteger (toInteger n)])
+    vec = P.fromList (reverse [1..fromInteger (toInteger n)])
 
 main = ndpMain "QSort"
                "[OPTION] ... SIZES ..."
@@ -33,6 +29,6 @@ run opts () sizes =
   case map read sizes of
     []  -> failWith ["No sizes specified"]
     szs -> do
-             benchmark opts qsortVect' (map generateVector szs) show
+             benchmark opts qsortVect (map generateVector szs) P.nf show
              return ()
 
