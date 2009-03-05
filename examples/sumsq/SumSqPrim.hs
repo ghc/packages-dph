5a02d8470673ae3e0df7e1d50611145c1fed1ea9
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2009-03-05 06:32:35 +0000

    Revamped sumsq benchmark

--- examples/sumsq/SumSqPrim.hs
+++ examples/sumsq/SumSqPrim.hs
@@ -1,6 +1,4 @@
--- the infamous sum square fusion example
-
-module Main (main)
+module SumSqPrim (sumSq)
 where
 
 import Data.Array.Parallel.Unlifted as U
@@ -8,6 +6,3 @@ import Data.Array.Parallel.Unlifted as U
 sumSq :: Int -> Int
 {-# NOINLINE sumSq #-}
 sumSq n = U.sum (U.map (\x -> x * x) (U.enumFromTo 1 n))
-
-main = print $ sumSq 100
-
