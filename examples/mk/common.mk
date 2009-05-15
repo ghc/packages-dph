23a1fb4f4be60b19513741c327cc8b667354d4b6
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2009-05-15 05:19:56 +0000

    Adapt examples makefile to new build locations

--- examples/mk/common.mk
+++ examples/mk/common.mk
@@ -1,13 +1,14 @@
 DPHDIR = $(TOPDIR)/..
 
-HC = $(DPHDIR)/../../ghc/stage2-inplace/ghc
-HCPKG = $(DPHDIR)/../../utils/ghc-pkg/install-inplace/bin/ghc-pkg
+HC = $(DPHDIR)/../../inplace/bin/ghc-stage2
+HCPKG = $(DPHDIR)/../../inplace/bin/ghc-pkg
 
 BENCH_DIR = $(TOPDIR)/lib
 BENCH_FLAGS = -package dph-bench -package-conf $(BENCH_DIR)/dph-bench.conf
 BENCH_DEP = $(BENCH_DIR)/dist/inplace-pkg-config
 
-DPH_FLAGS += -Odph -funfolding-use-threshold30 -funbox-strict-fields -fcpr-off -threaded
+DPH_FLAGS += -Odph -funfolding-use-threshold30 -funbox-strict-fields -fcpr-off \
+	     -threaded
 
 WAYS = seq par
 WAY_FLAGS = -fdph-$(WAY) -package dph-$(WAY) -odir $(WAY) -hidir $(WAY)
