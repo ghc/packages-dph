9477ff84636a73d904c0580a71a526dc206f4219
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2009-10-21 02:01:15 +0000

    Fiddle with inline phases

--- dph-base/include/fusion-phases.h
+++ dph-base/include/fusion-phases.h
@@ -2,7 +2,7 @@
 #define INLINE_UP      INLINE
 #define INLINE_STREAM  INLINE [1]
 #define INLINE_DIST    INLINE [1]
-#define INLINE_PA      INLINE [3]
+#define INLINE_PA      INLINE
 #define INLINE_BACKEND INLINE [2]
 
 #define PHASE_PA
