0f68cd51e3c8fabab372954c4d1fe18c59f71e33
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2009-10-30 01:47:19 +0000

    Use comprehensions but no new types in Quickhull

--- examples/quickhull/QuickHullVect.hs
+++ examples/quickhull/QuickHullVect.hs
@@ -12,38 +12,26 @@ import qualified Data.Array.Parallel.Prelude.Int as Int
 import qualified Prelude as P
 
 distance :: Point -> Line -> Double
-distance (Point xo yo) (Line (Point x1 y1) (Point x2 y2))
+distance (xo, yo) ((x1, y1), (x2, y2))
   = (x1-xo) * (y2 - yo) - (y1 - yo) * (x2 - xo)
 
 hsplit :: [:Point:] -> Line -> [:Point:]
-hsplit points line@(Line p1 p2)
+hsplit points line@(p1, p2)
   | lengthP packed Int.< 2 = singletonP p1 +:+ packed
   | otherwise
---  = concatP [: hsplit packed ends
---               | ends <- singletonP (Line p1 pm) +:+ singletonP (Line pm p2) :]
-  = concatP (mapP (\ends -> hsplit packed ends)
-                  (singletonP (Line p1 pm) +:+ singletonP (Line pm p2)))
+  = concatP [: hsplit packed ends | ends <- [:(p1, pm), (pm, p2):] :]
   where
---    cross  = [: distance p line | p <- points :]
-    cross  = mapP (\p -> distance p line) points
---    packed = [: p | (p,c) <- zipP points cross, c > 0.0 :]
-    packed = mapP (\(p, _) -> p) 
-                  (filterP (\(p, c) -> c > 0.0) (zipP points cross))
-
+    cross  = [: distance p line | p <- points :]
+    packed = [: p | (p,c) <- zipP points cross, c > 0.0 :]
     pm     = points !: maxIndexP cross
 
 quickHull' :: [:Point:] -> [:Point:]
 quickHull' points
   | lengthP points Int.== 0 = points
   | otherwise
---  = concatP [: hsplit points ends
---               | ends <- singletonP (Line minx maxx)
---                         +:+ singletonP (Line maxx minx) :]
-  = concatP (mapP (\ends -> hsplit points ends)
-                  (singletonP (Line minx maxx) +:+ singletonP (Line maxx minx)))
+  = concatP [: hsplit points ends | ends <- [: (minx, maxx), (maxx, minx) :] :]
   where
---    xs   = [: x | Point x y <- points :]
-    xs   = mapP (\(Point x y) -> x) points
+    xs   = [: x | (x, y) <- points :]
     minx = points !: minIndexP xs
     maxx = points !: maxIndexP xs
 
--- examples/quickhull/Types.hs
+++ examples/quickhull/Types.hs
@@ -1,29 +1,29 @@
 {-# LANGUAGE PArr #-}
 {-# OPTIONS -fvectorise #-}
 
-module Types ( Point(..), Line(..), points, xsOf, ysOf) where
+module Types ( Point, Line, points, xsOf, ysOf) where
 
 import Data.Array.Parallel.Prelude
 
-data Point = Point Double Double
-data Line  = Line  Point Point
+type Point = (Double, Double)
+type Line  = (Point, Point)
 
 points' :: [:Double:] -> [:Double:] -> [:Point:]
-points' xs ys = zipWithP Point xs ys
+points' = zipP
 
 points :: PArray Double -> PArray Double -> PArray Point
 {-# NOINLINE points #-}
 points xs ys = toPArrayP (points' (fromPArrayP xs) (fromPArrayP ys))
 
 xsOf' :: [:Point:] -> [:Double:]
-xsOf' ps = [: x | Point x _ <- ps :]
+xsOf' ps = [: x | (x, _) <- ps :]
 
 xsOf :: PArray Point -> PArray Double
 {-# NOINLINE xsOf #-}
 xsOf ps = toPArrayP (xsOf' (fromPArrayP ps))
 
 ysOf' :: [:Point:] -> [:Double:]
-ysOf' ps = [: y | Point _ y <- ps :]
+ysOf' ps = [: y | (_, y) <- ps :]
 
 ysOf :: PArray Point -> PArray Double
 {-# NOINLINE ysOf #-}
