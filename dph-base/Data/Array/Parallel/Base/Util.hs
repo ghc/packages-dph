c569f4c6f0165b6a121ccf022e55f09ba44ba4d5
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2009-11-02 13:00:32 +0000

    Make fromBool and toBool monomorphic
    They weren't getting inlined otherwise. Big performance ouch: this change
    makes sequential Quickhull about 30% faster.

--- dph-base/Data/Array/Parallel/Base/Util.hs
+++ dph-base/Data/Array/Parallel/Base/Util.hs
@@ -2,12 +2,12 @@ module Data.Array.Parallel.Base.Util (
   fromBool, toBool
 ) where
 
-fromBool :: Num a => Bool -> a
+fromBool :: Bool -> Int
 fromBool False = 0
 fromBool True  = 1
 {-# INLINE fromBool #-}
 
-toBool :: Num a => a -> Bool
+toBool :: Int -> Bool
 toBool n | n == 0    = False
          | otherwise = True
 {-# INLINE toBool #-}
