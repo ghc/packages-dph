24328beb79d2ac16a1eea7160ab5df4c4c4e7b16
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2010-05-27 09:05:00 +0000

    Use -rtsopts when building test cases

--- examples/mk/common.mk
+++ examples/mk/common.mk
@@ -8,7 +8,7 @@ BENCH_FLAGS = -package dph-bench -package-conf $(BENCH_DIR)/dph-bench.conf
 BENCH_DEP = $(BENCH_DIR)/dist/inplace-pkg-config
 
 DPH_FLAGS += -Odph -funfolding-use-threshold30 -funbox-strict-fields -fcpr-off \
-	     -threaded
+	     -threaded -rtsopts
 
 WAYS = seq par
 WAY_FLAGS = -fdph-$(WAY) -package dph-$(WAY) -odir $(WAY) -hidir $(WAY)
