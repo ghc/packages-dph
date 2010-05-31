9abb4b9a92087c82125f3b45b6ec1c6f5c387855
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2010-05-31 03:04:22 +0000

    Test data generator for Quickhull

--- examples/quickhull/TestData.hs
+++ examples/quickhull/TestData.hs
@@ -1,4 +1,4 @@
-
+{-# LANGUAGE TypeOperators #-}
 module TestData 
 	( genPointsUniform
 	, genPointsDisc
@@ -12,6 +12,7 @@ import qualified Data.Array.Parallel.Prelude 	    as P
 import qualified Data.Array.Parallel.Prelude.Double as D
 import qualified Data.Array.Parallel.PArray         as P
 import Data.Array.Parallel.PArray		    (PArray)
+import Data.Array.Parallel.Base ( (:*:)(..) )
 
 import System.Random
 import Control.Exception
@@ -27,64 +28,66 @@ seed 		= 42742
 -- | Some uniformly distributed points
 genPointsUniform 
 	:: Int			-- ^ number of points
-	-> Double		-- ^ minimum coordinate
-	-> Double		-- ^ maximum coordinate
-	-> [(Double, Double)]
+	-- -> Double		-- ^ minimum coordinate
+	-- -> Double		-- ^ maximum coordinate
+        -> U.Array (Double :*: Double)
+	-- -> [(Double, Double)]
 
-genPointsUniform n minXY maxXY
+genPointsUniform n -- minXY maxXY
  = let
-	pointMin	= 10
-	pointMax	= 510
+	pointMin	= -100
+	pointMax	= 100
 	gen		= mkStdGen seed
-   in	toPairs $ take (2*n) $ randomRs (pointMin, pointMax) gen
-
-toPairs []        = []
-toPairs (x:y:pts) = (x, y) : toPairs pts
-
+        pts             = U.randomRs (n*2) (pointMin, pointMax) gen
+        xs              = U.extract pts 0 n
+        ys              = U.extract pts n n
+   in
+   U.zip xs ys
 
 -- | Some points distributed as a disc
-genPointsDisc 
+genPointsDisc'
 	:: Int			-- ^ number of points
 	-> (Double, Double) 	-- ^ center of disc
 	-> Double 		-- ^ radius of disc
-	-> [(Double, Double)]
+        -> U.Array (Double :*: Double)
+	-- -> [(Double, Double)]
 
-genPointsDisc n (originX, originY) radiusMax
+genPointsDisc' n (originX, originY) radiusMax
  = let	(genRadius, genAngle)		
 		= split $ mkStdGen seed
 	
-	radius	= take n $ randomRs (0, radiusMax) genRadius 
-	angle	= take n $ randomRs (- pi, pi) genAngle
+	-- radius	= take n $ randomRs (0, radiusMax) genRadius 
+	-- angle	= take n $ randomRs (- pi, pi) genAngle
+	radius = U.randomRs n (0, radiusMax) genRadius
+        angle  = U.randomRs n (-pi,pi) genAngle
 
-	makeXY (r, a)	
-	 	= ( originX + r * cos a
-	   	  , originY + r * sin a)	
+	makeXY r a	
+	 	= (originX + r * cos a)
+	   	  :*: (originY + r * sin a)	
+    in
+    originX `seq` originY `seq` U.zipWith makeXY radius angle
 
-   in	map makeXY $ zip radius angle	
-
+genPointsDisc :: Int -> U.Array (Double :*: Double)
+genPointsDisc n = genPointsDisc' n (0,0) 100
 
 -- | A point cloud with areas of high an low density
 genPointsCombo 
 	:: Int 			-- ^ number of points
-	-> [(Double, Double)]	
+        -> U.Array (Double :*: Double)
 
 genPointsCombo n
- 	=  genPointsDisc (n `div` 5) (250, 250) 200
-	++ genPointsDisc (n `div` 5) (100, 100) 80 
-	++ genPointsDisc (n `div` 5) (150, 300) 30 
-	++ genPointsDisc (n `div` 5) (500, 120) 30 
-	++ genPointsDisc (n `div` 5) (300, 200) 150
-
+ 	=  genPointsDisc' (n `div` 5) (250, 250) 200
+	U.+:+ genPointsDisc' (n `div` 5) (100, 100) 80 
+	U.+:+ genPointsDisc' (n `div` 5) (150, 300) 30 
+	U.+:+ genPointsDisc' (n `div` 5) (500, 120) 30 
+	U.+:+ genPointsDisc' (n `div` 5) (300, 200) 150
 
 -- | Convert a list of points to a PArray
-toPArrayPoints :: [(Double, Double)] -> IO (PArray QH.Point)
+toPArrayPoints :: U.Array (Double :*: Double) -> IO (PArray QH.Point)
 toPArrayPoints ps
- = do	let pts	= QH.points	(P.fromList (map fst ps))
-	   			(P.fromList (map snd ps))
-	evaluate $ force pts
-	return pts
+  = do
+      let pts = QH.points (P.fromUArrPA' (U.fsts ps))
+                          (P.fromUArrPA' (U.snds ps))
+      evaluate $ P.nf pts
+      return pts
 
--- | Force points to be evaluated
-force pts 
-	= P.toUArrPA (QH.xsOf pts) U.!: 0 D.+ 
-          P.toUArrPA (QH.ysOf pts) U.!: 0
