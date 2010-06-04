690cd117412ae1872fd8f47e0951f19078e9639a
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2010-06-04 04:54:14 +0000

    Add benchmarking version of delaunay

--- examples/delaunay/Main.hs
+++ examples/delaunay/Main.hs
@@ -1,21 +1,36 @@
-import Types
+import qualified Types as T
 import Delaunay
 
 import qualified Data.Array.Parallel.Unlifted as U
 import qualified Data.Array.Parallel.Prelude  as P
-import qualified Data.Array.Parallel.PArray as PA
+import Data.Array.Parallel.PArray as PA
 
-import SVG
+import Bench.Benchmark
+import Bench.Options
 
-import System.Environment
 import System.IO
+import Control.Exception (evaluate)
 
-main = do
-         [arg] <- getArgs
-         h <- openBinaryFile arg ReadMode
-         pts <- U.hGet h
-         let points = P.fromUArrPA_2' pts
-             edges  = PA.toList (delaunayPoints points)
-         putStrLn $ svg (PA.toList points) edges
+loadPoints :: String -> IO (Point (PArray T.Point))
+loadPoints file
+  = do
+      h <- openBinaryFile file ReadMode
+      pts <- U.hGet h
+      hClose h
+      let points = P.fromUArrPA_2' pts
+      evaluate $ nf points
+      return $ ("N = " ++ show (U.length pts)) `mkPoint` points
 
+main = ndpMain "Delaunay"
+               "[OPTION] ... FILE"
+               run [] ()
+
+run opts () [] = failWith ["No sizes or input files specified"]
+run opts () args =
+  do
+    benchmark opts delaunayPoints
+        (map loadPoints args)
+        nf
+        (\ps -> "Result length = " ++ show (PA.length ps))
+    return ()
 
