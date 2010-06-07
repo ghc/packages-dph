77bbf07ab5feafa339ab32056cca31c17df38d28
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2010-06-07 03:03:27 +0000

    More delaunay optimisations

--- examples/delaunay/Sort.hs
+++ examples/delaunay/Sort.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE PArr #-}
 {-# OPTIONS -fvectorise #-}
-module Sort ( kthSmallest, median, collect, sort ) where
+module Sort ( median, medianIndex, collect, sort ) where
 
 import Data.Array.Parallel.Prelude
 import qualified Data.Array.Parallel.Prelude.Double as D
@@ -25,22 +25,36 @@ kthSmallest xs k
 median :: [:D.Double:] -> D.Double
 median xs = kthSmallest xs (lengthP xs `div` 2)
 
-union :: [:(Int,[:Int:]):] -> [:(Int,[:Int:]):]
-union ps
-  | lengthP ps I.<= 1 = ps
+kthSmallestIndex :: [:D.Double:] -> Int -> Int -> Int
+kthSmallestIndex xs k i
+  | k >= lengthP lts && k < n - lengthP gts = i + lengthP lts
+  | otherwise = kthSmallestIndex ys k' i'
+  where
+    n     = lengthP xs
+    pivot = xs !: (n `div` 2)
+
+    lts   = [:x | x <- xs, x D.< pivot:]
+    gts   = [:x | x <- xs, x D.> pivot:]
+
+    (ys, k', i') | k < lengthP lts = (lts, k, i)
+                 | otherwise       = (gts, k - (n - lengthP gts), i + (n - lengthP gts))
+
+medianIndex :: [:D.Double:] -> Int
+medianIndex xs = kthSmallestIndex xs (lengthP xs `div` 2) 0
+
+collect :: [:(Int,Int):] -> [:(Int,[:Int:]):]
+collect ps
+  | lengthP ps I.== 0 = [::]
   | otherwise =
   let
     (pivot,_) = ps !: (lengthP ps `I.div` 2)
-    ls        = [:(p,xs) | (p,xs) <- ps, p I.< pivot:]
-    gs        = [:(p,xs) | (p,xs) <- ps, p I.> pivot:]
-    eqs       = [:xs     | (p,xs) <- ps, p I.== pivot:]
+    ls        = [:(i,j) | (i,j) <- ps, i I.< pivot:]
+    gs        = [:(i,j) | (i,j) <- ps, i I.> pivot:]
+    js        = [:j | (i,j) <- ps, i I.== pivot:]
 
-    ss        = mapP union [:ls,gs:]
+    ss        = mapP collect [:ls,gs:]
   in
-  (ss!:0) +:+ [:(pivot, concatP eqs):] +:+ (ss!:1)
-
-collect :: [:(Int,Int):] -> [:(Int,[:Int:]):]
-collect ps = union [:(i,[:j:]) | (i,j) <- ps:]
+  (ss!:0) +:+ [:(pivot,js):] +:+ (ss!:1)
 
 sort :: [:Int:] -> [:Int:]
 sort xs | lengthP xs I.<= 1 = xs
