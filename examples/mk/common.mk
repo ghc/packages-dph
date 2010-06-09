f3fe16f2a6c5e53878871964f89c4e8f867d4cf0
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2010-06-09 07:51:58 +0000

    Prune DPH_FLAGS
    CPR doesn't seem to panic on type families any longer so we don't need
    -fcpr-off. We always use {-# UNPACK #-} pragmas so -funbox-strict-fields is
    redundant. I think we needed -funfolding-use-threshold30 for join points but
    they seem to work ok now.

--- examples/mk/common.mk
+++ examples/mk/common.mk
@@ -7,8 +7,7 @@ BENCH_DIR = $(TOPDIR)/lib
 BENCH_FLAGS = -package dph-bench -package-conf $(BENCH_DIR)/dph-bench.conf
 BENCH_DEP = $(BENCH_DIR)/dist/inplace-pkg-config
 
-DPH_FLAGS += -Odph -funfolding-use-threshold30 -funbox-strict-fields -fcpr-off \
-	     -threaded -rtsopts
+DPH_FLAGS += -Odph -threaded -rtsopts
 
 WAYS = seq par
 WAY_FLAGS = -fdph-$(WAY) -package dph-$(WAY) -odir $(WAY) -hidir $(WAY)
