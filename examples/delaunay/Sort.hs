57c7f661564b3d8641212803f39c29300ba74429
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2010-06-11 01:40:55 +0000

    Fix median in delaunay

--- examples/delaunay/Sort.hs
+++ examples/delaunay/Sort.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE PArr #-}
 {-# OPTIONS -fvectorise #-}
-module Sort ( median, medianIndex, collect, sort ) where
+module Sort ( medianIndex, collect, sort ) where
 
 import Data.Array.Parallel.Prelude
 import qualified Data.Array.Parallel.Prelude.Double as D
@@ -8,44 +8,27 @@ import Data.Array.Parallel.Prelude.Int as I
 
 import qualified Prelude as P
 
-kthSmallest :: [:D.Double:] -> Int -> D.Double
-kthSmallest xs k
-  | k >= lengthP lts && k < n - lengthP gts = pivot
-  | otherwise = kthSmallest ys k'
+kthSmallestIndex :: [:(Int,D.Double):] -> Int -> Int
+kthSmallestIndex xs k
+  | k >= lengthP lts && k < n - lengthP gts = i
+  | otherwise = (kthSmallestIndex ys k')
   where
     n     = lengthP xs
-    pivot = xs !: (n `div` 2)
+    (i,x) = xs !: (n `div` 2)
 
-    lts   = [:x | x <- xs, x D.< pivot:]
-    gts   = [:x | x <- xs, x D.> pivot:]
+    lts   = [:(j,y) | (j,y) <- xs, y D.< x:]
+    gts   = [:(j,y) | (j,y) <- xs, y D.> x:]
 
     (ys, k') | k < lengthP lts = (lts, k)
              | otherwise       = (gts, k - (n - lengthP gts))
 
-median :: [:D.Double:] -> D.Double
-median xs = kthSmallest xs (lengthP xs `div` 2)
-
-kthSmallestIndex :: [:D.Double:] -> Int -> Int -> Int
-kthSmallestIndex xs k i
-  | k >= lengthP lts && k < n - lengthP gts = i + lengthP lts
-  | otherwise = kthSmallestIndex ys k' i'
-  where
-    n     = lengthP xs
-    pivot = xs !: (n `div` 2)
-
-    lts   = [:x | x <- xs, x D.< pivot:]
-    gts   = [:x | x <- xs, x D.> pivot:]
-
-    (ys, k', i') | k < lengthP lts = (lts, k, i)
-                 | otherwise       = (gts, k - (n - lengthP gts), i + (n - lengthP gts))
-
 medianIndex :: [:D.Double:] -> Int
-medianIndex xs = kthSmallestIndex xs (lengthP xs `div` 2) 0
+medianIndex xs = (kthSmallestIndex (indexedP xs) (lengthP xs `div` 2))
 
 collect :: [:(Int,Int):] -> [:(Int,[:Int:]):]
 collect ps
   | lengthP ps I.== 0 = [::]
-  | otherwise =
+  | otherwise = (
   let
     (pivot,_) = ps !: (lengthP ps `I.div` 2)
     ls        = [:(i,j) | (i,j) <- ps, i I.< pivot:]
@@ -54,11 +37,12 @@ collect ps
 
     ss        = mapP collect [:ls,gs:]
   in
-  (ss!:0) +:+ [:(pivot,js):] +:+ (ss!:1)
+  (ss!:0) +:+ [:(pivot,sort js):] +:+ (ss!:1)
+  )
 
 sort :: [:Int:] -> [:Int:]
 sort xs | lengthP xs I.<= 1 = xs
-sort xs = (ss!:0) +:+ [:pivot:] +:+ (ss!:1)
+sort xs = ((ss!:0) +:+ [:pivot:] +:+ (ss!:1))
   where
     pivot = xs !: (lengthP xs `I.div` 2)
     ls    = [:x | x <- xs, x < pivot:]
