897cf56e5035892a6afbbbd764247f9350dfa41c
benl@ouroborus.net <unknown>
2010-10-18 05:53:07 +0000

    Add dotp to dph-examples

---
+++ dph-examples/imaginary/DotProduct/dph/DotProductVect.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE PArr #-}
+{-# OPTIONS -fvectorise #-}
+module DotProductVect ( dotPA ) where
+
+import Data.Array.Parallel.Prelude
+import Data.Array.Parallel.Prelude.Double as D
+
+import qualified Prelude
+
+dotPA :: PArray Double -> PArray Double -> Double
+{-# NOINLINE dotPA #-}
+dotPA v w = dotp' (fromPArrayP v) (fromPArrayP w)
+
+dotp' :: [:Double:] -> [:Double:] -> Double
+dotp' v w = D.sumP (zipWithP (*) v w)
---
+++ dph-examples/imaginary/DotProduct/dph/Main.hs
@@ -0,0 +1,44 @@
+
+import DotProductVect
+import Timing
+import Randomish
+import System.Environment
+import Data.Vector.Unboxed		(Vector)
+import Data.Array.Parallel.Prelude	as P
+import Data.Array.Parallel.PArray	as P
+import qualified Data.Vector.Unboxed	as V
+
+main :: IO ()
+main 
+ = do	args	<- getArgs
+	case args of
+	  [alg, len] 	-> run alg (read len) 
+	  _		-> usage
+	
+run alg len
+ = do	let vec1 = randomishDoubles len 0 1 1234
+	let vec2 = randomishDoubles len 0 1 12345
+
+	(result, tElapsed) <- runAlg alg vec1 vec2
+
+	putStr	$ prettyTime tElapsed
+	putStr	$ (take 12 $ show result) ++ "\n"
+
+runAlg "dph" vec1 vec2 
+ = do	let arr1 = fromUArrPA' vec1
+	let arr2 = fromUArrPA' vec2
+	arr1 `seq` arr2 `seq` return ()
+
+	time	$ let result	= dotPA arr1 arr2
+		  in  result `seq` return result
+
+runAlg "vector" vec1 vec2
+ = do	vec1 `seq` vec2 `seq` return ()
+
+	time	$ let result	= V.sum $ V.zipWith (*) vec1 vec2
+		  in  result `seq` return result
+		
+usage
+ = do	putStr 	$  "usage: dotp <alg> <length>"
+ 		++ "  alg one of " ++ show ["dph", "vector"]
+
