58321ebf5cd2754b70a19260f4175612cc92495e
benl@ouroborus.net <unknown>
2010-10-26 06:13:48 +0000

    nbody: tweak default parameters

--- dph-examples/real/BarnesHut/MainArgs.hs
+++ dph-examples/real/BarnesHut/MainArgs.hs
@@ -30,8 +30,8 @@ mainArgs
 	, Arg	{ argIndex	= ArgWindowSize
 		, argAbbr	= Nothing
 		, argName	= Just "window"
-		, argData	= argDataDefaulted "Int" ArgtypeInt 500
-		, argDesc	= "Size of window in pixels (default 500)" }
+		, argData	= argDataDefaulted "Int" ArgtypeInt 800
+		, argDesc	= "Size of window in pixels (default 800)" }
 
 	, Arg	{ argIndex	= ArgSolver
 		, argAbbr	= Nothing
@@ -49,41 +49,41 @@ mainArgs
 		, argAbbr	= Just 't'
 		, argName	= Just "timestep"
 		, argData	= argDataDefaulted "Double" ArgtypeDouble 1
-		, argDesc	= "Time step between stats (default 1)" }
+		, argDesc	= "Time step between states (default 1)" }
 
 	, Arg	{ argIndex	= ArgRate
 		, argAbbr	= Just 'r'
 		, argName	= Just "rate"
-		, argData	= argDataDefaulted "Double" ArgtypeInt 100
-		, argDesc	= "Number of simulation steps per second of real time (default 100)" }
+		, argData	= argDataDefaulted "Double" ArgtypeInt 50
+		, argDesc	= "Number of simulation steps per second of real time (default 50)" }
 
 	, Arg	{ argIndex	= ArgBodyCount
 		, argAbbr	= Just 'b'
 		, argName	= Just "bodies"
-		, argData	= argDataDefaulted "Int" ArgtypeInt 100 
-		, argDesc	= "Number of bodies in simulation (default 100)" }
+		, argData	= argDataDefaulted "Int" ArgtypeInt 200 
+		, argDesc	= "Number of bodies in simulation (default 200)" }
 
 	, Arg	{ argIndex	= ArgBodyMass
 		, argAbbr	= Just 'm'
 		, argName	= Just "mass"
-		, argData	= argDataDefaulted "Double" ArgtypeDouble 1 
-		, argDesc	= "Mass of each body (default 1)" }
+		, argData	= argDataDefaulted "Double" ArgtypeDouble 10
+		, argDesc	= "Mass of each body (default 10)" }
 
 	, Arg	{ argIndex	= ArgEpsilon
 		, argAbbr	= Just 'e'
 		, argName	= Just "epsilon"
-		, argData	= argDataDefaulted "Double" ArgtypeDouble 5
-		, argDesc	= "Ignore forces between bodies closer than this (default 5)" }
+		, argData	= argDataDefaulted "Double" ArgtypeDouble 100
+		, argDesc	= "Smoothing parameter (default 100)" }
 		
 	, Arg	{ argIndex	= ArgDiscSize
 		, argAbbr	= Just 'd'
 		, argName	= Just "disc"
-		, argData	= argDataDefaulted "Double" ArgtypeDouble 100
-		, argDesc	= "Starting size of disc containing bodies (default 100)" }
+		, argData	= argDataDefaulted "Double" ArgtypeDouble 50
+		, argDesc	= "Starting size of disc containing bodies (default 50)" }
 
 	, Arg	{ argIndex	= ArgStartSpeed
 		, argAbbr	= Just 's'
 		, argName	= Just "speed"
-		, argData	= argDataDefaulted "Double" ArgtypeDouble 0.15
-		, argDesc	= "Starting speed of bodies (default 0.15)" }
+		, argData	= argDataDefaulted "Double" ArgtypeDouble 0.5
+		, argDesc	= "Starting rotation speed of bodies (default 0.5)" }
 	]
