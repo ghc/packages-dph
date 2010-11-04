e0de6ff93dd72fb3cbf6f7fe657fa56c6a4eb2a8
benl@ouroborus.net <unknown>
2010-11-04 02:19:52 +0000

    Update names of benchmarks

--- dph-buildbot/src/Benchmarks.hs
+++ dph-buildbot/src/Benchmarks.hs
@@ -11,63 +11,63 @@ benchmarksDPH config
  =	
  	[ -- dot product
 	  bench config
-		"dph-dotp"
+		"dph.dotp.vectorised.par.N4"
 		"dph-examples/dist/build/dph-dotp/dph-dotp dph 10000000 +RTS -N4"
 
 	, bench config
-		"dph-dotp-seq"
-		"dph-examples/dist/build/dph-dotp-seq/dph-dotp-seq dph 10000000"		
+		"dph.dotp.vectorised.seq.N4"
+		"dph-examples/dist/build/dph-dotp-seq/dph-dotp-seq dph 10000000 +RTS -N4"		
 
 	, bench config
-		"dph-dotp[vector-seq]"
+		"dph.dotp.vector.seq.N4"
 		"dph-examples/dist/build/dph-dotp/dph-dotp vector 10000000 +RTS -N4"
 
 	  -- sum of squares
 	, bench config
-		"dph-sumsq"
+		"dph.sumsq.vectorised.par.N4"
 		"dph-examples/dist/build/dph-sumsq/dph-sumsq dph 100000000 +RTS -N4"
 
 	, bench config
-		"dph-sumsq-seq"
-		"dph-examples/dist/build/dph-sumsq-seq/dph-sumsq-seq dph 100000000"
+		"dph.sumsq.vectorised.seq.N4"
+		"dph-examples/dist/build/dph-sumsq-seq/dph-sumsq-seq dph 100000000 +RTS -N4"
 
 	, bench config
-		"dph-sumsq[vector-seq]"
+		"dph.sumsq.vector.seq.N4"
 		"dph-examples/dist/build/dph-sumsq/dph-sumsq vector 100000000 +RTS -N4"
 		
 	  -- evens
         , bench config
-		"dph-evens"
+		"dph.evens.vectorised.par.N4"
 		"dph-examples/dist/build/dph-evens/dph-evens 10000000 +RTS -N4"
 
         , bench config
-		"dph-evens-seq"
-		"dph-examples/dist/build/dph-evens-seq/dph-evens-seq 10000000"
+		"dph.evens.vectorised.seq.N4"
+		"dph-examples/dist/build/dph-evens-seq/dph-evens-seq 10000000 +RTS -N4"
 	
 	  -- quicksort
 	, bench config 
-		"dph-quicksort"
+		"dph.quicksort.vectorised.par.N4"
 		"dph-examples/dist/build/dph-quicksort/dph-quicksort 100000 +RTS -N4"
 
 	  -- quickhull 
 	, bench config 
-		"dph-quickhull"
+		"dph.quickhull.vectorised.par.N4"
 		"dph-examples/dist/build/dph-quickhull/dph-quickhull 1000000 +RTS -N4 -K20M"
 
 	, bench config 
-		"dph-quickhull-seq"
-		"dph-examples/dist/build/dph-quickhull-seq/dph-quickhull-seq 1000000 +RTS -K40M"
+		"dph.quickhull.vectorised.seq.N4"
+		"dph-examples/dist/build/dph-quickhull-seq/dph-quickhull-seq 1000000 +RTS -N4 -K40M"
 
 	, bench config
-		"dph-quickhull[vector-seq]"
+		"dph.quickhull.vector.seq.N4"
 		"dph-examples/dist/build/dph-quickhull-vector/dph-quickhull-vector vector 1000000 +RTS -N4"
 
 	, bench config
-		"dph-quickhull[vector-forkIO]"
+		"dph.quickhull.vector-forkIO.par.N4"
 		"dph-examples/dist/build/dph-quickhull-vector/dph-quickhull-vector io 1000000 +RTS -N4"
 
 	, benchUp config
-	 	"dph-quickhull[c-seq]"
+	 	"dph.quickhull.c.seq"
 		(inDir "dph-examples/spectral/QuickHull/c" $ qssystem "make")
 		"dph-examples/spectral/QuickHull/c/quickhull 1000000"				
 	]
@@ -78,11 +78,11 @@ benchmarksRepa :: Config -> [Benchmark]
 benchmarksRepa config
  =	-- mmult
 	[ bench config
-		"repa-mmult"
+		"repa.mmult.par.N4"
 		"repa-examples/dist/build/repa-mmult/repa-mmult -random 1024 1024 -random 1024 1024 +RTS -N4"
 	
 	, benchUp config
-		"repa-mmult[c-seq]"
+		"repa.mmult.c.seq"
 		(inDir "repa-examples/MMult/legacy" $ qssystem "make")
 		"repa-examples/MMult/legacy/mmult -random 1024 1024 -random 1024 1024"
 	
@@ -92,7 +92,7 @@ benchmarksRepa config
 		inputgz	= input ++ ".gz"
 
 	  in  	benchUp config
-		 	"repa-laplace"
+		 	"repa.laplace.par.N4"
 			(do	ensureDir "output"
 				check $ HasExecutable laplace
 				whenM (test $ HasFile inputgz)
@@ -101,7 +101,7 @@ benchmarksRepa config
 			(laplace ++ " 1000 " ++ input ++ " output/laplace.bmp +RTS -N4 -qg")
 
 	, benchUp config
-		"repa-laplace[c-seq]"
+		"repa.laplace.c.seq"
 		(inDir "repa-examples/Laplace/legacy" $ qssystem "make")
 		"repa-examples/Laplace/legacy/laplace 400 400 1000 output/laplace_c-seq.ppm"
 
@@ -111,7 +111,7 @@ benchmarksRepa config
 		inputgz	= input ++ ".gz"
 		
 	  in	benchUp config
-			"repa-blur"
+			"repa.blur.par.N4"
 			(do	ensureDir "output"
 				check $ HasExecutable blur
 				whenM (test $ HasFile inputgz)
@@ -124,7 +124,7 @@ benchmarksRepa config
 		inputgz	   = input ++ ".gz"
 		
 	  in	benchUp config
-			"repa-edgedetect"
+			"repa.edgedetect.par.N2"
 			(do	ensureDir "output"
 				check $ HasExecutable edgedetect
 				whenM (test $ HasFile inputgz)
@@ -137,7 +137,7 @@ benchmarksRepa config
 		inputgz	= input ++ ".gz"
 		
 	  in	benchUp config
-			"repa-fft2d"
+			"repa.fft2d.par.N4"
 			(do	ensureDir "output"
 				check $ HasExecutable fft2d
 				whenM (test $ HasFile inputgz)
@@ -146,7 +146,7 @@ benchmarksRepa config
 
 	-- fft3d-highpass
 	, benchUp config
-		"repa-fft3d"
+		"repa.fft3d.par.N4"
 		(ensureDir "output/fft3d")
 		"repa-examples/dist/build/repa-fft3d-highpass/repa-fft3d-highpass 128 output/fft3d/slice +RTS -N4 -qg"
 	]
