bc5a883078c1e362b5f0026381870f4d396f0e27
Ben Lippmeier <benl@ouroborus.net>
2010-12-06 04:28:58 +0000

    Add comments to mksm

--- examples/smvm/mksm.c
+++ examples/smvm/mksm.c
@@ -7,15 +7,28 @@
 
 #include <HsFFI.h>
 
+// Number of columns in the matrix.
 HsInt cols;
+
+// Number of rows in the matrix.
 HsInt rows;
+
+// Ratio of non-zero to zero values in the matrix.
 double ratio;
 
+// The length of each row in the matrix.
 HsInt *lengths;
+
+// Index buffer.
+// Used when generating the indices for each row.
 HsInt *indices;
 
+// We can make matricies of floats or doubles.
 enum { FLOAT, DOUBLE } type;
 
+
+// Write some random non-zero doubles to a file.
+// Returns the sum of all the values.
 HsDouble gen_doubles( int file, HsInt n )
 {
   HsDouble d;
@@ -39,6 +52,9 @@ HsDouble gen_doubles( int file, HsInt n )
   return sum;
 }
 
+
+// Write some random, non-zero floats to a file.
+// Returns the sum of all the values.
 HsFloat gen_floats( int file, HsInt n )
 {
   HsFloat d;
@@ -63,11 +79,16 @@ HsFloat gen_floats( int file, HsInt n )
 }
 
 
+// Generate lengths of each of the rows in the matrix.
+// Returns the total number of elements.
 HsInt gen_lengths()
 {
   HsInt i;
   HsInt n = 0;
 
+  // Set the maximum number of elements for the row.
+  // Doing it this way give us between  0 and COLS  elems for 
+  // a ratio of 0.5. Ratios of > 0.5 don't work.
   int range = ((double)cols * 2) * ratio;
   
   for( i = 0; i < rows; ++i ) {
@@ -78,6 +99,8 @@ HsInt gen_lengths()
   return n;
 }
 
+
+// Check if an index has already been used.
 int find_index( int from, int to, HsInt idx )
 {
   while( from != to ) {
@@ -87,6 +110,8 @@ int find_index( int from, int to, HsInt idx )
   return 0;
 }
 
+
+// Comparison function for HsInt.
 int cmp_HsInt( const void *p, const void *q )
 {
   HsInt x = *(HsInt *)p;
@@ -97,27 +122,40 @@ int cmp_HsInt( const void *p, const void *q )
   return 0;
 }
 
+
+// Generate indices for a single row of the sparse matrix,
+// and write them to the given file.
 void gen_indices( int file )
 {
-  HsInt i, j, k;
+  HsInt i, j;
 
-  k = 0;
   for( i = 0; i < rows; ++i ) {
+
+    // We want an index for each of the (packed) elements in the row.
     for( j = 0; j < lengths[i]; ++j ) {
+
+      // Keep looping around until we find an index that hasn't already been used.
+      // NOTE: This sucks up most of the runtime.
       do {
         indices[j] = random() % cols;
       } while( find_index( 0, j, indices[j] ) );
     }
+    
+    // Sort the indices so they're in accending order.
     qsort( indices, j, sizeof(HsInt), cmp_HsInt );
+
+    // Write indices out to file.
     write( file, indices, sizeof(HsInt) * j );
   }
 }
+
 
 int usage()
 {
   puts( "mksm [float|double] COLS ROWS RATIO FILE" );
   exit(1);
 }
+
 
 int main( int argc, char *argv[] )
 {
@@ -145,14 +183,19 @@ int main( int argc, char *argv[] )
       type = DOUBLE;
     else
     {
-      fputs( "Invalid type\n", stderr );
+      fputs( "mksm: Invalid type\n", stderr );
       usage();
     }
   }
 
-  cols = atoi( argv[arg++] );
-  rows = atoi( argv[arg++] );
+  cols  = atoi( argv[arg++] );
+  rows  = atoi( argv[arg++] );
+
   ratio = atof( argv[arg++] );
+  if (ratio > 0.5) {
+	fputs ("mksm: RATIO must be < 0.5\n", stderr);
+	exit(1);
+  }
    
   lengths = (HsInt *)malloc( rows * sizeof(HsInt) );
   indices = (HsInt *)malloc( cols * sizeof(HsInt) );
@@ -163,25 +206,40 @@ int main( int argc, char *argv[] )
   file = creat( argv[arg], 0666 );
 
   n = gen_lengths();
-  write( file, &rows, sizeof(rows) );
+
+  // Number of rows in the matrix.
+  write( file, &rows,   sizeof(rows) );
+
+  // How many elements are in each row.
   write( file, lengths, sizeof(HsInt) * rows );
+
+  // Indices of all the elements.
   write( file, &n, sizeof(n) );
   gen_indices( file );
+
+  // Values of the matrix elements.
   write( file, &n, sizeof(n) );
   if( type == DOUBLE )
     sum1 = gen_doubles(file, n);
   else
     sum1 = (HsDouble)gen_floats(file, n);
+
+  // The dense vector.
   write( file, &cols, sizeof(cols) );
   if( type == DOUBLE )
     sum2 = gen_doubles(file, cols);
   else
     sum2 = (HsDouble)gen_floats(file, n);
+
+  // Close the output file.
   close(file);
 
-  printf( "columns = %d; rows = %d; elements = %d (%d)\n", (int)cols, (int)rows, (int)n,
-           (int)(type == FLOAT ? sizeof(HsFloat) : sizeof(HsDouble)) );
-  printf( "%Lf %Lf\n", (long double)sum1, (long double)sum2 );
+  printf( "columns = %d; rows = %d; elements = %d (%d)\n",
+		(int)cols, (int)rows, (int)n,
+                (int)(type == FLOAT ? sizeof(HsFloat) : sizeof(HsDouble)) );
+  printf( "sum of matrix elements %Lf\n", (long double)sum1 );
+  printf( "sum of vector elements %Lf\n", (long double)sum2 );
+
   return 0;
 }
 
