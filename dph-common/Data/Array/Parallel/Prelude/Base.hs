53fc9fba5609aa1222190a3e7eaa4aedcfc7963c
Ben Lippmeier <benl@ouroborus.net>
2011-05-11 18:05:55 +1000

    Remove bogus, unused modules

--- dph-common/Data/Array/Parallel/Prelude/Base.hs
+++
@@ -1,8 +0,0 @@
-module Data.Array.Parallel.Prelude.Base (
-  -- module Data.Array.Parallel.Prelude.Base.PArr
-  -- , module Data.Array.Parallel.Lifted
-) where
-
--- import Data.Array.Parallel.Prelude.Base.PArr
--- import Data.Array.Parallel.Lifted
-
--- dph-common/Data/Array/Parallel/Prelude/Base/PArr.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE ParallelArrays #-}
-module Data.Array.Parallel.Prelude.Base.PArr (
-  -- FIXME: REMOVE THIS MODULE: clients should better imported these from Data.Array.Parallel
-  module Data.Array.Parallel
-) where
-
-import Data.Array.Parallel
