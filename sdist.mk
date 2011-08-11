6ac04b583e2709df4c8d7bab335c1c0bee1c7af2
Ben Lippmeier <benl@ouroborus.net>
2011-08-11 18:18:36 +1000

    Hacks on sdist.mk

--- sdist.mk
+++ sdist.mk
@@ -1,22 +1,20 @@
 # The following works in a GHC build tree.  To use the installed GHC
 # instead, replace with GHC=ghc.
-GHC=../../../inplace/bin/ghc-stage2
+GHC=ghc
 
 # The "-pkg" suffix is to avoid clashing with the dph-seq and dph-par
 # directories created by the GHC build system.
 
-all : dph-seq-pkg dph-par-pkg sdist
+all : dph-seq dph-par sdist
 
 define dph_create
 
-dph-$1-pkg :
-	rm -rf dph-$1-pkg dph-$1-pkg.tmp
-	cp -r dph-common dph-$1-pkg.tmp
-	rm dph-$1-pkg.tmp/dph-common.cabal
-	sed -e "s/DPHWAY/$1/g;/HS-Source-Dirs/d" dph-common/dph-common.cabal > dph-$1-pkg.tmp/dph-$1-pkg.cabal
-	sed "s/common/$1/g" dph-common/ghc.mk > dph-$1-pkg.tmp/ghc.mk
-	sed "s/common/$1/g" dph-common/GNUmakefile > dph-$1-pkg.tmp/GNUmakefile
-	mv dph-$1-pkg.tmp dph-$1-pkg
+dph-$1 :
+	rm -rf dph-$1 dph-$1.tmp
+	cp -r dph-common dph-$1.tmp
+	rm dph-$1.tmp/dph-common.cabal
+	sed -e "s/DPHWAY/$1/g;/HS-Source-Dirs/d" dph-common/dph-common.cabal > dph-$1.tmp/dph-$1.cabal
+	mv dph-$1.tmp dph-$1
 
 endef
 
@@ -24,11 +22,11 @@ $(eval $(call dph_create,seq))
 $(eval $(call dph_create,par))
 
 clean :
-	rm -rf dph-seq-pkg dph-par-pkg
+	rm -rf dph-seq dph-par
 
-PACKAGES = dph-base dph-prim-interface dph-prim-seq dph-prim-par dph-seq-pkg dph-par-pkg dph
+PACKAGES = dph-base dph-prim-interface dph-prim-seq dph-prim-par dph-seq dph-par dph
 
-sdist : dph-seq-pkg dph-par-pkg
+sdist : dph-seq dph-par
 	for i in $(PACKAGES); do \
 	    ( cd $$i; \
 	      cabal configure --with-ghc=$(GHC); \
