a78b1dc836affed063c1476f4481c69db132fa99
Ben Lippmeier <benl@ouroborus.net>
2011-10-10 13:56:14 +1100

    Add D.A.P.U.Sequential module to tie together the sequential backend

---
+++ dph-prim-seq/Data/Array/Parallel/Unlifted/Sequential.hs
@@ -0,0 +1,12 @@
+
+module Data.Array.Parallel.Unlifted.Sequential
+        ()
+where
+import Data.Array.Parallel.Unlifted.Sequential.Basics
+import Data.Array.Parallel.Unlifted.Sequential.Combinators
+import Data.Array.Parallel.Unlifted.Sequential.Sums
+import Data.Array.Parallel.Unlifted.Sequential.USegd    ()
+import Data.Array.Parallel.Unlifted.Sequential.USel     ()
+import Data.Array.Parallel.Unlifted.Sequential.USSegd   ()
+import Data.Array.Parallel.Unlifted.Sequential.UVSegd   ()
+import Data.Array.Parallel.Unlifted.Sequential.Vector
