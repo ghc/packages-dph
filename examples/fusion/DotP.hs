b12766ece1925e0f0c22322f2d5ce70f9615dec8
Ben Lippmeier <benl@ouroborus.net>
2012-01-23 15:40:19 +1100

    Delete old fusion tests

--- examples/fusion/DotP.hs
+++
@@ -1,8 +0,0 @@
-module DotP where
-import Data.Array.Parallel.Unlifted
-
--- > 1 loopU/loopU
-
-dotp :: UArr Double -> UArr Double -> Double
-dotp v w = sumU (zipWithU (*) v w)
-
--- examples/fusion/Map_Map_Replicate.hs
+++
@@ -1,9 +0,0 @@
-module Map_Map_Replicate where
-import Data.Array.Parallel.Unlifted
-
--- > 2 loopU/loopU
-
-map_map_replicate :: (UA a, UA b, UA c)
-                  => (b -> c) -> (a -> b) -> Int -> a -> UArr c
-map_map_replicate f g n = mapU f . mapU g . replicateU n
-
