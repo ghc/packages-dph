2c384be18af830e7f27593b572ba2136656d1479
Ben Lippmeier <benl@ouroborus.net>
2012-01-23 15:40:19 +1100

    Delete old fusion tests

--- examples/fusion/Map_Map.hs
+++
@@ -1,8 +0,0 @@
-module Map_Map where
-import Data.Array.Parallel.Unlifted
-
--- > 1 loopU/loopU
-
-map_map :: (Int -> Int) -> (Int -> Int) -> UArr Int -> UArr Int
-map_map f g = mapU f . mapU g
-
--- examples/fusion/Map_Replicate.hs
+++
@@ -1,8 +0,0 @@
-module Map_Replicate where
-import Data.Array.Parallel.Unlifted
-
--- > 1 loopU/loopU
-
-map_replicate :: (UA a, UA b) => (a -> b) -> Int -> a -> UArr b
-map_replicate f n = mapU f . replicateU n
-
